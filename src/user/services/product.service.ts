d24b958a3cf1a4d66df48573f105a9e0056247e7
leonard <leonard9619@gmail.com>
2022-04-12 12:14:42 +0200

    changes

--- src/user/services/product.service.ts
+++ src/user/services/product.service.ts
@@ -27,7 +27,6 @@ export class ProductService {
     static async update(product) {
         const productRepository = getRepository(Product);
         let oldProduct = await productRepository.findOne(product.id);
-        console.log({ oldProduct, product })
         product = productRepository.merge(oldProduct, { raw: JSON.stringify(product), offer: JSON.stringify(product.offers), brand: JSON.stringify(product.brand), name: product.name, description: product.description, mpn: product.mpn, sku: product.sku, image: product.image });
         return productRepository.save(product);
     }
